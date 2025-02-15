a2cd886262f2b255db7a4f4e8700b55ae0d8826a
Naravula  Devaraj <devarajhansi974@gmail.com>
2025-02-15 06:19:23 +0000

    hi api

---
+++ dev/api.js
@@ -0,0 +1,7 @@
+// {
+//     "name":"devaraj",
+//     "price":"90",
+//     "t_name":"dev"
+// }
+
+// api may be object form or array form
---
+++ dev/de_str_array.js
@@ -0,0 +1,12 @@
+const course={
+    name:"dev",
+    price:"999",
+    tname:"dev"
+}
+
+//course.name
+// for de structure objects and also we can do array
+const { name:n}=course
+
+// console.log(name);
+console.log(n)
