2efac47c403f144c084954f41f516348f7a66a36
Naravula  Devaraj <devarajhansi974@gmail.com>
2025-02-19 12:44:02 +0000

    commit

---
+++ control_flow_04/one.js
@@ -0,0 +1,43 @@
+//  if
+const isuser=false
+// if (isuser){
+//     console.log("hi")
+
+// }
+// else{
+// console.log("bye")}
+
+// const time=30
+// if  (time>10){
+//     const m="hi "
+//     console.log(m)
+// }
+// //console.log(m)
+
+const b=2000
+
+// if(b>100) console.log("fine"),
+// console.log("bye")
+if(b>100000){
+    console.log("less than 100000")
+}
+else if(b>1000){
+    console.log("less than 2000")
+}
+else {
+    console.log("less than 200")
+}
+
+
+const use=true
+const dc=true
+const lfg=false 
+const lfe=true
+
+if(use&&dc && 2==2){
+    console.log("go head")
+}
+
+if(lfg || lfe){
+    console.log("user loged in")
+}
--- control_flow_o4/one.js
+++
@@ -1,6 +0,0 @@
-//  if
-const isuser=true
-if (2=="2"){
-    console.log("hi")
-
-}
